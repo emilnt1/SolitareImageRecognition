effbb13c2b3f0cf847e048b820acafdef2c6e2b2
harzz <harzz3k@gmail.com>
2022-04-06 08:41:00 +0200

    Lavet en masse klasser til logikdelen

---
+++ Model/Board.py
@@ -0,0 +1,12 @@
+import Column as column
+import Foundation as foundation
+import DrawPile as drawpile
+import Deck as deck
+
+class Board:
+    columns = [column, column, column, column, column, column, column]
+    foundations = [foundation, foundation, foundation, foundation]
+    drawPile = drawpile
+    deck = deck
+
+    
---
+++ Model/Foundation.py
@@ -0,0 +1,23 @@
+from Model.CardLogic import CardLogic
+from Model.Card import Card
+
+
+class Foundation(CardLogic):
+    cards = []
+
+    def __init__(self):
+        super().__init__()
+        self.cards = []
+
+    def pop(self, cards):
+        if len(self.cards) > 1:
+            pass
+        else:
+            return self.cards.pop()
+
+    def push(self, cards):
+        if len(cards) < 1:
+            if self.cards[len(cards)].rank == cards[0].rank - 1:
+                self.cards.extend(cards)
+            else:
+                pass
