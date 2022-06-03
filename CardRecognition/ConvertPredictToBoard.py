276195900a5a28a130209af4263cfb40e5a6aa00
Armandas Rokas <armandas.rokas@gmail.com>
2022-06-03 15:09:17 +0200

    convertion to board integrated to UI

--- CardRecognition/ConvertPredictToBoard.py
+++ CardRecognition/ConvertPredictToBoard.py
@@ -8,8 +8,7 @@ from Model.Foundation import *
 from Model.SuitType import SuitType as type
 import numpy as np
 
-def convertToMatrix(det, names):
-
+def convertPredictToBoard(det, names):
     upper_cards = []
     lower_cards = []
 
@@ -17,12 +16,9 @@ def convertToMatrix(det, names):
     for *xyxy, conf, cls in reversed(det):
         xyxy_array = np.array(xyxy)
         c = int(cls)
-        card = Card()
-        card.suit = convertSuitStrToEnum(names[c][0]) # TODO: Should it be number?
-        card.rank = names[c][1:]
+        card = Card(int(names[c][1:]), convertSuitStrToEnum(names[c][0]))
         card.x = xyxy_array[0]
         card.y = xyxy_array[1]
-     #   card.conf = conf.item()
 
         # Opdele til foundations and columner. 
         if xyxy_array[1] <= 100:
@@ -31,66 +27,42 @@ def convertToMatrix(det, names):
             lower_cards.append(card)
 
     # Divide lower cards to columns
-    column1 = Column()
-    column2 = Column()
-    column3 = Column()
-    column4 = Column()
-    column5 = Column()
-    column6 = Column()
-    column7 = Column()
+
+    board = Board(DrawPile(), Deck())
     for lower_card in lower_cards:
         if lower_card.x <= 50.0:
-            column1.cards.append(lower_card)
+            board.columns[0].cards.append(lower_card)
         elif lower_card.x > 50.0 and lower_card.x <= 140.0:
-            column2.cards.append(lower_card)
+            board.columns[1].cards.append(lower_card)
         elif lower_card.x > 140.0 and lower_card.x <= 230.0:
-            column3.cards.append(lower_card)
+            board.columns[2].cards.append(lower_card)
         elif lower_card.x > 230.0 and lower_card.x <= 320.0:
-            column4.cards.append(lower_card)
+            board.columns[3].cards.append(lower_card)
         elif lower_card.x > 320.0 and lower_card.x <= 420.0:
-            column5.cards.append(lower_card)
+            board.columns[4].cards.append(lower_card)
         elif lower_card.x > 420.0 and lower_card.x <= 510.0:
-            column6.cards.append(lower_card)
+            board.columns[5].cards.append(lower_card)
         elif lower_card.x > 510:
-            column7.cards.append(lower_card)    
-
-
-    board = Board()
-    board.columns.append(column1)
-    board.columns.append(column2)
-    board.columns.append(column3)
-    board.columns.append(column4)
-    board.columns.append(column5)
-    board.columns.append(column6)
-    board.columns.append(column7)
+            board.columns[6].cards.append(lower_card)  
 
     for c in board.columns:
         c.cards.sort(key=lambda x : x.y)
 
     # Add foundations and DrawPile
-    foundation1 = Foundation()
-    foundation2 = Foundation()
-    foundation3 = Foundation()
-    foundation4 = Foundation()
 
     for upper_card in upper_cards:
             if upper_card.x <= 230.0:
                 board.drawPile.cards.append(upper_card)
             elif upper_card.x > 230.0 and upper_card.x <= 330.0:
-                foundation1.cards.append(upper_card)
+                board.foundations[0].cards.append(upper_card)
             elif upper_card.x > 330.0 and upper_card.x <= 420.0:
-                foundation2.cards.append(upper_card)
+                board.foundations[1].cards.append(upper_card)
             elif upper_card.x > 420.0 and upper_card.x <= 510.0:
-                foundation3.cards.append(upper_card)
+                board.foundations[2].cards.append(upper_card)
             elif upper_card.x > 510.0:
-                foundation4.cards.append(upper_card)
+                board.foundations[3].cards.append(upper_card)
                 
-
-    board.foundations.append(foundation1)
-    board.foundations.append(foundation2)
-    board.foundations.append(foundation3)
-    board.foundations.append(foundation4)
-
+    return board
     
 def convertSuitStrToEnum(suit):
     return {
@@ -99,16 +71,3 @@ def convertSuitStrToEnum(suit):
         'R' : type.D,
         'K' : type.C
     }.get(suit)
-
-
-    # match suit:
-    #     case 'H':
-    #         return type.H
-    #     case 'S':
-    #         return type.S
-    #     case 'R':
-    #         return type.D
-    #     case 'K':
-    #         return type.C 
-
-
