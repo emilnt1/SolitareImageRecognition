c7ee860e8fd052fa2864dbc93aa67f0abd5eba52
Armandas Rokas <armandas.rokas@gmail.com>
2022-06-07 13:55:28 +0200

    added stateful board

--- AI/AI.py
+++ AI/AI.py
@@ -7,16 +7,21 @@ from Model.SuitType import concludeFromString
 from Model.SuitType import SuitType
 from Model.GameRules import *
 
+## Statefull board
+stateful_board = Board()
+
 def nextMove(board):
+    ## Merge board fra ML and stateful_board
+
     ##isAceFound, count_columns,count_foundation, card = findAce(board)
-    curr_move = findAce(board)
+    curr_move = putFoundation(board)
     if ( len(curr_move) != 0):
         return  curr_move
 
     
 
 
-def findAce(board):
+def putFoundation(board):
     count_columns = 0
     for c in board.columns:
         count_columns += 1
@@ -26,6 +31,9 @@ def findAce(board):
             if c.cards:
                 if allowedMoveFoundation(c.cards[-1], foundation):
                     #return "Move " +  str(c.cards[-1].rank) + str(c.cards[-1].suit) +  "  from column: " + str(count_columns) + " to foundation:" + str(count_foundation) + "." 
+                    #board.moveCard(c, c.cards[-1], foundation)
+                    #stateful_board = b
+                    stateful_board.foundations[count_foundation-1].append
                     return "Move " + str(c.cards[-1]) +   " from column: " + str(count_columns) + " to foundation: " + str(count_foundation) + "." 
                 #return (True, count_columns,count_foundation, c.cards[-1])
     return ""        
