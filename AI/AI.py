adb156b97c5facd6c2e5380acc8057f86f834419
Armandas Rokas <armandas.rokas@gmail.com>
2022-06-09 15:08:06 +0200

    bugfix: the most hidden cards

--- AI/AI.py
+++ AI/AI.py
@@ -105,7 +105,7 @@ def putColumn(board):
                     continue
 
                 if allowedMoveColumn(c, column_to):
-                    possibleFromColumn.append(c, count_columns_from, count_columns_to, board.getCardsLeftColumn[count_columns_from-1])
+                    possibleFromColumn.append(c, count_columns_from, count_columns_to, board.getCardsLeftColumn(count_columns_from-1))
                     #stateful_board.cardsLeftColumns[count_columns_from-1] -= 1
                     #if(c.rank == 13):
                     #    stateful_board.columns[count_columns_to-1].isKingMovedTo = True
