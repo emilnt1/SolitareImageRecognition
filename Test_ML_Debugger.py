ae47ce667e6c06d42effa2d9843c28770e6e313e
Armandas Rokas <armandas.rokas@gmail.com>
2022-06-13 12:44:42 +0200

    test debugger

---
+++ Test_ML_Debugger.py
@@ -0,0 +1,29 @@
+from Model.Debugger import *
+from Model.Card import Card
+from Model.DrawPile import DrawPile
+from Model.CardFactory import CardFactory
+from View.KabaleView import display
+from Model.Board import Board
+from Model.SuitType import concludeFromString
+from Model.SuitType import SuitType as type
+from Model.GameRules import *
+from Model.DrawPile import DrawPile
+from Model.Deck import Deck
+from Model.Column import Column
+from AI.AI import *
+
+
+
+def testBasicBoard():
+    b1 = Board(DrawPile(), None)
+    b1.columns[0].cards.append(Card(2,type.D))
+    b1.columns[1].cards.append(Card(1,type.D))
+    b1.columns[2].cards.append(Card(1,type.H))
+    b1.columns[3].cards.append(Card(1,type.C))
+    b1.columns[4].cards.append(Card(1,type.S))
+    b1.columns[5].cards.append(Card(2,type.S))
+    b1.columns[6].cards.append(Card(3,type.S))
+    b1.drawPile.cards.append(Card(4,type.S))
+    display(b1)
+
+testBasicBoard()
